c67b47ef5c92874148cb656909cc3ced036b0eab
velaphidelivers <velaphiv917@gmail.com>
2025-02-22 15:37:22 +0100

    we try again

---
+++ src/BridgeAPI/Models/TokenResponse.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+
+namespace Models;
+public class TokenResponse
+{
+    [JsonProperty("systemSecurityToken")]
+    public SystemSecurityToken? SystemSecurityToken { get; set; }
+}
+
+public class SystemSecurityToken
+{
+    [JsonProperty("token")]
+    public string? Token { get; set; }
+
+    [JsonProperty("expiryInSeconds")]
+    public int ExpiryInSeconds { get; set; }
+}
