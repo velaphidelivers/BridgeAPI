c5afac7e4dfab863cbaeaaa4efa410749a3e4d1c
velaphidelivers <velaphiv917@gmail.com>
2025-02-22 15:12:56 +0100

    I have no idea what am I doing

--- src/BridgeAPI/Middleware/RoutingMiddleware.cs
+++ src/BridgeAPI/Middleware/RoutingMiddleware.cs
@@ -1,15 +1,20 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
 public class RoutingMiddleware
 {
     private readonly RequestDelegate _next;
-    private readonly ILogger<RoutingMiddleware> _logger;
-    public RoutingMiddleware(RequestDelegate next, ILogger<RoutingMiddleware> logger)
+    private readonly ILogger<CorrelationIdMiddleware> _logger;
+
+    public RoutingMiddleware(RequestDelegate next, ILogger<CorrelationIdMiddleware> logger)
     {
         _next = next;
         _logger = logger;
     }
+
     public async Task InvokeAsync(HttpContext context)
     {
-      //  await context.Response.WriteAsync(string.Empty);
         // Continue with the next middleware or endpoint
         await _next(context);
     }
